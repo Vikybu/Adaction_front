53a6ba096babd8245b6b876a00b60f2ec01d6c54
Victoria Ferry <ferryvictoria@hotmail.fr>
2025-10-03 11:38:09 +0200

    add: creation of the componenet MenuCompo and add toapp.vue

---
+++ src/assets/elements.js
@@ -0,0 +1,86 @@
+export let menuItemVolunteer = [
+  {
+    svg: `<svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 26 26"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="lucide lucide-sprout-icon lucide-sprout"
+      >
+        <path
+          d="M14 9.536V7a4 4 0 0 1 4-4h1.5a.5.5 0 0 1 .5.5V5a4 4 0 0 1-4 4 4 4 0 0 0-4 4c0 2 1 3 1 5a5 5 0 0 1-1 3"
+        />
+        <path d="M4 9a5 5 0 0 1 8 4 5 5 0 0 1-8-4" />
+        <path d="M5 21h14" />
+      </svg>`,
+    nameElement: 'Dashboard',
+    hrefElement: '#/dashboard',
+  },
+  {
+    svg: `<svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 26 26"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="lucide lucide-package-icon lucide-package"
+      >
+        <path
+          d="M11 21.73a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73z"
+        />
+        <path d="M12 22V12" />
+        <polyline points="3.29 7 12 12 20.71 7" />
+        <path d="m7.5 4.27 9 5.15" />
+      </svg>`,
+    nameElement: 'Collectes',
+    hrefElement: '#/collectes',
+  },
+  {
+    svg: `<svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 26 26"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="lucide lucide-heart-icon lucide-heart"
+      >
+        <path
+          d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5"
+        />
+      </svg>`,
+    nameElement: 'Dons',
+    hrefElement: '#/donation',
+  },
+  {
+    svg: `<svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 26 26"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="lucide lucide-user-icon lucide-user"
+      >
+        <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+        <circle cx="12" cy="7" r="4" />
+      </svg>`,
+    nameElement: 'Profil',
+    hrefElement: '#/profil',
+  },
+]
