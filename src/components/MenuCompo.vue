1e263b65f8d48d18f8a692113b5144d370f55a40
Juliette <jblanc17@outlook.fr>
2025-10-17 11:42:54 +0200

    fix menuCompo

--- src/components/MenuCompo.vue
+++ src/components/MenuCompo.vue
@@ -17,14 +17,14 @@ const { svg, nameElement, page } = defineProps({
 
 <template>
  <RouterLink
-  :to="props.page"
+  :to="page"
   class="flex flex-col items-center justify-center w-28 px-1.5 py-2 rounded-lg
          text-gray-600 hover:text-emerald-700 hover:bg-emerald-50 transition-colors duration-200
          mx-1 sm:mx-2 text-center"
   active-class="text-emerald-700 bg-emerald-100"
 >
-  <span v-html="props.svg" class="w-5 h-5"></span>
-  <span class="text-sm font-medium mt-1">{{ props.nameElement }}</span>
+  <span v-html="svg" class="w-5 h-5"></span>
+  <span class="text-sm font-medium mt-1">{{ nameElement }}</span>
 </RouterLink>
 
 </template>
